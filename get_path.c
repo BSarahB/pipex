b6d9cda6b5f0cc50c2d359bb54c71a741fb2addd
BSarahB <55717377+BSarahB@users.noreply.github.com>
2021-11-15 22:42:58 +0100

    norm OK

--- get_path.c
+++ get_path.c
@@ -11,77 +11,35 @@
 /* ************************************************************************** */
 #include "pipex.h"
 
-char	**ft_create_tab(char const *s, char c, size_t words_nbr)
+char	**ft_get_argv_cmd(int i, char **argv)
 {
-	char		**tab;
-	size_t		i;
-	size_t		k;
-	size_t		wd_length;
+	char	**argv_cmd;
+	char	slash[2];
 
-	i = 0;
-	k = 0;
-	tab = malloc(sizeof(*tab) * (words_nbr + 1));
-	if (!tab)
-		return (NULL);
-	while (k < words_nbr)
+	slash[0] = '/';
+	slash[1] = 0;
+	argv_cmd = ft_split(argv[i], ' ');
+	ft_update_string(&argv_cmd[0], ft_strjoin(slash, argv_cmd[0]));
+	return (argv_cmd);
+}
+
+int	ft_execve_join(t_struct *ptr, char **envp, char **argv_cmd)
+{
+	char	*path_cmd_joined;
+	int		i;
+	int		exec_return;
+
+	exec_return = 0;
+	i = -1;
+	while ((*ptr).path_tab[++i])
 	{
-		while (s[i] && s[i] == c)
-			i++;
-		wd_length = 0;
-		while ((s[i + wd_length]) && (s[i + wd_length]) != c)
-			wd_length++;
-		tab[k] = ft_strndup((char *)&s[i], wd_length);
-		k++;
-		i = i + wd_length;
+		path_cmd_joined = ft_strjoin((*ptr).path_tab[i], argv_cmd[0]);
+		exec_return = execve(path_cmd_joined, argv_cmd, envp);
+		free(path_cmd_joined);
+		if (exec_return != -1)
+			break ;
 	}
-	tab[k] = 0;
-	return (tab);
-}
-
-size_t	ft_words_nbr(const char *s, char c)
-{
-	size_t	i;
-	size_t	k;
-	size_t	words_nbr;
-
-	i = 0;
-	words_nbr = 0;
-	while (s[i])
-	{
-		while (s[i] && s[i] == c)
-			i++;
-		k = 0;
-		while (s[i + k] && (s[i + k]) != c)
-			k++;
-		if (k != 0)
-			words_nbr++;
-		i = i + k;
-	}
-	return (words_nbr);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**tab;
-	size_t	words_nbr;
-
-	if (!s)
-		return (NULL);
-	words_nbr = ft_words_nbr(s, c);
-	tab = ft_create_tab(s, c, words_nbr);
-	return (tab);
-}
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	if (n == 0)
-		return (0);
-	while (i < n - 1 && s1[i] && s2[i] && s1[i] == s2[i])
-		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (exec_return);
 }
 
 char	**ft_get_path(char **envp)
