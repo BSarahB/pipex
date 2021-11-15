23355ce3f8304b9c1134821c25cd1296826ea4d2
BSarahB <55717377+BSarahB@users.noreply.github.com>
2021-11-15 15:16:38 +0100

    fixing argv_absolute_command return and execution

--- create_fork.c
+++ create_fork.c
@@ -28,26 +28,32 @@ int	ft_parent_process(t_struct *ptr, char **argv, char **envp)
 	char	*path_cmd2_joined;
 	int		i;
 	char	**argv_cmd2;
+	char	**argv_abs_cmd2;
 	int		exec_return;
 
 	exec_return = 0;
 	i = -1;
-	argv_cmd2 = ft_get_argv_cmd(3, argv);
+	argv_abs_cmd2 = ft_split(argv[3], ' ');
+	argv_cmd2 = ft_get_argv_cmd(3, argv);//proteger ici if argv_cmd == NULL
 	close (STDIN_FILENO);
 	(void)dup((*ptr).p[0]);
 	close ((*ptr).p[0]);
 	close ((*ptr).p[1]);
 	dup2((*ptr).fd2, STDOUT_FILENO);
-	while ((*ptr).path_tab[++i])
-	{
-		path_cmd2_joined = ft_strjoin((*ptr).path_tab[i], argv_cmd2[0]);
-		exec_return = execve(path_cmd2_joined, argv_cmd2, envp);
-		free(path_cmd2_joined);
-		if (exec_return != -1)
-			break ;
+	if (execve(argv_abs_cmd2[0], argv_abs_cmd2, envp) == -1)//if (abs_exceve) n a pas fonctionne :
+	{//on execute la while de path join
+		while ((*ptr).path_tab[++i])
+		{
+			path_cmd2_joined = ft_strjoin((*ptr).path_tab[i], argv_cmd2[0]);
+			exec_return = execve(path_cmd2_joined, argv_cmd2, envp);
+			free(path_cmd2_joined);
+			if (exec_return != -1)
+				break ;
+		}
 	}
 	if (exec_return == -1 && (errno == 2 || errno == 13))
 			(*ptr).errnum = 127;
+	ft_free_tab(&argv_abs_cmd2);
 	ft_free_tab(&argv_cmd2);
 	return (exec_return);
 }
@@ -57,27 +63,33 @@ int	ft_child_process(t_struct *ptr, char **argv, char **envp)
 	char	*path_cmd1_joined;
 	int		j;
 	char	**argv_cmd1;
+	char	**argv_abs_cmd1;
 	int		exec_return;
 
 	exec_return = 0;
 	j = -1;
+	argv_abs_cmd1 = ft_split(argv[2], ' ');
 	argv_cmd1 = ft_get_argv_cmd(2, argv);
 	close (STDOUT_FILENO);
 	dup2((*ptr).fd1, STDIN_FILENO);
 	(void)dup((*ptr).p[1]);
 	close ((*ptr).p[1]);
 	close ((*ptr).p[0]);
-	while ((*ptr).path_tab[++j])
+	if (execve(argv_abs_cmd1[0], argv_abs_cmd1,envp) == -1)//if (abs_exceve) n a pas fonctionne :
 	{
-		path_cmd1_joined = ft_strjoin((*ptr).path_tab[j], argv_cmd1[0]);
-		exec_return = execve(path_cmd1_joined, argv_cmd1, envp);
-		errno;
-		free(path_cmd1_joined);
-		if (exec_return != -1)
-			break ;
+		while ((*ptr).path_tab[++j])
+		{
+			path_cmd1_joined = ft_strjoin((*ptr).path_tab[j], argv_cmd1[0]);
+			exec_return = execve(path_cmd1_joined, argv_cmd1, envp);
+			errno;
+			free(path_cmd1_joined);
+			if (exec_return != -1)
+				break ;
+		}
 	}
 	if (exec_return == -1 && (errno == 2 || errno == 13))
 			(*ptr).errnum = 127;
+	ft_free_tab(&argv_abs_cmd1);
 	ft_free_tab(&argv_cmd1);
 	return (exec_return);
 }
